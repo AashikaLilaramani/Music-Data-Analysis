0f3f34a292b55882e801e1868145a893125f0249
Aashika Lilaramani <aashika.lilaramani@gmail.com>
2025-04-18 15:09:37 -0500

    Finished classes for objecttier

--- objecttier.py
+++ objecttier.py
@@ -105,6 +105,268 @@ class Customer:
    @property
    def SupportRepId(self):
       return self._SupportRepId
+   
+##################################################################
+# Employee Class
+# Constructor(...)
+# Properties:
+# EmployeeId: int, LastName: string, FirstName: string, Title: string, ReportsTo: int, BirthDate: datetime, HireDate: datetime, Address: string, City: string, State: string, Country: string, PostalCode: string, Phone: string, Fax: string, Email: string
+# Employee class definition (constructors & properties)
+class Employee:
+   def __init__(self, Employee_Id, Last_Name, First_Name, Emp_Title, Reports_To, Birth_Date, Hire_Date, Emp_Address, Emp_City, Emp_State, Emp_Country, Postal_Code, Emp_Phone, Emp_Fax, Emp_Email):
+      self._EmployeeId = Employee_Id
+      self._LastName = Last_Name
+      self._FirstName = First_Name
+      self._Title = Emp_Title
+      self._ReportsTo = Reports_To
+      self._BirthDate = Birth_Date
+      self._HireDate = Hire_Date
+      self._Address = Emp_Address
+      self._City = Emp_City
+      self._State = Emp_State
+      self._Country = Emp_Country
+      self._PostalCode = Postal_Code
+      self._Phone = Emp_Phone
+      self._Fax = Emp_Fax
+      self._Email = Emp_Email
+    
+   @property
+   def EmployeeId(self):
+      return self._EmployeeId
+   @property
+   def LastName(self):
+      return self._LastName
+   @property
+   def FirstName(self):
+      return self._FirstName
+   @property
+   def Title(self):
+      return self._Title
+   @property
+   def ReportsTo(self):
+      return self._ReportsTo
+   @property
+   def Address(self):
+      return self._Address
+   @property
+   def City(self):
+      return self._City
+   @property
+   def State(self):
+      return self._State
+   @property
+   def Country(self):
+      return self._Country
+   @property
+   def PostalCode(self):
+      return self._PostalCode
+   @property
+   def Phone(self):
+      return self._Phone
+   @property
+   def Fax(self):
+      return self._Fax
+   @property
+   def Email(self):
+      return self._Email
+##################################################################
+# Genre Class
+# Constructor(...)
+# Properties:
+# GenreId: int, Name: string
+# Genre class definition (constructors & properties)
+class Genre:
+   def __init__(self, Genre_Id, Genre_Name):
+      self._GenreId = Genre_Id
+      self._Name = Genre_Name
+   
+   # Properties
+   @property
+   def GenreId(self):
+      return self._GenreId
+   @property
+   def Name(self):
+      return self._Name
+##################################################################
+# Invoice Items Class
+# Constructor(...)
+# Properties:
+# InvoiceLineId: int, InvoiceId: int, TrackId: int, UnitPrice: int, Quantity: int
+# Invoice_Items class definition (constructors & properties)
+class Invoice_Items:
+   def __init__(self, Inv_Line_Id, Inv_Id, Track_Id, Unit_Price, Inv_Quantity):
+      self._InvoiceLineId = Inv_Line_Id
+      self._InvoiceId = Inv_Id
+      self._TrackId = Track_Id
+      self._UnitPrice = Unit_Price
+      self._Quantity = Inv_Quantity
+   
+   # Properties
+   @property
+   def InvoiceLineId(self):
+      return self._InvoiceLineId
+   @property
+   def InvoiceId(self):
+      return self._InvoiceId
+   @property
+   def TrackId(self):
+      return self._TrackId
+   @property
+   def UnitPrice(self):
+      return self._UnitPrice
+   @property
+   def Quantity(self):
+      return self._Quantity
+##################################################################
+# Invoice Class
+# Constructor(...)
+# Properties:
+# InvoiceId: int, CustomerId: int, InvoiceDate: datetime, BillingAddress: string, BillingState: string, BillingCountry: string, BillingPostalCode: string, Total: int
+# Invoice class definition (constructors & properties)
+class Invoice:
+   def __init__(self, Inv_Id, Cust_Id, Inv_Date, Address, City, State, Country, PostalCode, Inv_Total):
+      self._InvoiceId = Inv_Id
+      self._CusomterId = Cust_Id
+      self._InvoiceDate = Inv_Date
+      self._BillingAddress = Address
+      self._BillingCity = City
+      self._BillingState = State
+      self._BillingCountry = Country
+      self._BillingPostalCode = PostalCode
+      self._Total = Inv_Total
+   
+   # Properties
+   @property
+   def InvoiceId(self):
+      return self._InvoiceId
+   @property
+   def CustomerId(self):
+      return self._CusomterId
+   @property
+   def InvoiceDate(self):
+      return self._InvoiceDate
+   @property
+   def BillingAddress(self):
+      return self._BillingAddress
+   @property
+   def BillingCity(self):
+      return self._BillingState
+   @property
+   def BillingState(self):
+      return self._BillingState
+   @property
+   def BillingCountry(self):
+      return self._BillingCountry
+   @property
+   def BillingPostalCode(self):
+      return self._BillingPostalCode
+   @property
+   def Total(self):
+      return self._Total
+##################################################################
+# Media Types Class
+# Constructor(...)
+# Properties:
+# MediaTypeId: int, Name: int
+# Media_Types class definition (constructors & properties)
+class Media_Types:
+   def __init__(self, Media_TypeId, Media_Name):
+      self._MediaTypeId = Media_TypeId
+      self._Name = Media_Name
+   
+   # Properties
+   @property
+   def MediaTypeId(self):
+      return self._MediaTypeId
+   @property
+   def Name(self):
+      return self._Name
+##################################################################
+# PLaylist Track Class
+# Constructor(...)
+# Properties:
+# PlaylistId: int, TrackId, int
+# Playlist_Track class definition (constructors & properties)
+class Playlist_Track:
+   def __init__(self, Playlist_Id, Track_Id):
+      self._PlaylistId = Playlist_Id
+      self._TrackId = Track_Id
+   
+   # Properties
+   @property
+   def PlaylistId(self):
+      return self._PlaylistId
+   @property
+   def TrackId(self):
+      return self._TrackId
+##################################################################
+# PLaylist Class
+# Constructor(...)
+# Properties:
+# PlaylistId: int, Name: string
+# Playlist class definition (constructors & properties)
+class Playlist_Track:
+   def __init__(self, Playlist_Id, Playlist_Name):
+      self._PlaylistId = Playlist_Id
+      self._Name = Playlist_Name
+   
+   # Properties
+   @property
+   def PlaylistId(self):
+      return self._PlaylistId
+   @property
+   def Name(self):
+      return self._Name 
+##################################################################
+# Tracks Class
+# Constructor(...)
+# Properties:
+# TrackId: int, Name: string, AlbumId: int, MediaTypeId: int, GenreId: int, Composer: string, Milliseconds: int, Bytes: int, UnitPrice: int
+# Tracks class definition (constructors & properties)
+class Invoice:
+   def __init__(self, Track_Id, Track_Name, Album_Id, Media_TypeId, Genre_Id, Track_Composer, Milli_Seconds, Track_Bytes, Unit_Price):
+      self._TrackId = Track_Id
+      self._Name = Track_Name
+      self._AlbumId = Album_Id
+      self._MediaTypeId = Media_TypeId
+      self._GenreId = Genre_Id
+      self._Composer = Track_Composer
+      self._Milliseconds = Milli_Seconds
+      self._Bytes = Track_Bytes
+      self._UnitPrice = Unit_Price
+   
+   # Properties
+   @property
+   def TrackId(self):
+      return self._TrackId
+   @property
+   def Name(self):
+      return self._Name
+   @property
+   def AlbumId(self):
+      return self._AlbumId
+   @property
+   def MediaTypeId(self):
+      return self._MediaTypeId
+   @property
+   def GenreId(self):
+      return self._GenreId
+   @property
+   def Composer(self):
+      return self._Composer
+   @property
+   def Milliseconds(self):
+      return self._Milliseconds
+   @property
+   def Bytes(self):
+      return self._Bytes
+   @property
+   def UnitPrice(self):
+      return self._UnitPrice
+
+
+
+
 ##################################################################
 # 
 # num_albums:
--- presentationtier.py
+++ presentationtier.py
@@ -16,17 +16,103 @@ def print_stats(dbConn):
     print(f" Number of unique Artists in Database: {num_uniqueArtists:,}")
     menu_selection(dbConn)
 
+def music_tracks(dbConn):
+    print("Select an option: ")
+    print("  1. Print the top 10 most expensive tracks")
+    print("  2. Print the average track duration")
+    print("  3. Print the amount of tracks in each genre")
+    cmd = input("Your choice --> ")
+    print()
+
+
+    if(cmd == '1'):
+        print("Top 10 Most Expensive Tracks:")
+    elif(cmd == '2'):
+        print("Average Track Duration (in seconds):")
+    elif(cmd == '3'):
+        print("Amount of Tracks in Each Genre:")
+    else:
+        print("Unknown input. Try again.")
+        menu_selection(dbConn)
+
+
+
+def albums_artists(dbConn):
+    print("Select an option: ")
+    print("  1. Print albums by artist")
+    print("  2. Print most prolific artists")
+    cmd = input("Your choice --> ")
+    print()
+
+
+    if(cmd == '1'):
+        print("Albums by Artist:")
+    elif(cmd == '2'):
+        print("Most Prolific Artists:")
+    else:
+        print("Unknown input. Try again.")
+        menu_selection(dbConn)
+
+def sales_revenue(dbConn):
+    print("Select an option: ")
+    print("  1. Print total revenue by country")
+    print("  2. Print top 10 highest spending customers")
+    print("  3. Print monthly sales trends")
+    cmd = input("Your choice --> ")
+    print()
+
+
+    if(cmd == '1'):
+        print("Total Revenue By Country:")
+    elif(cmd == '2'):
+        print("Top 10 Highest Spending Customers:")
+    elif(cmd == '3'):
+        print("Monthly Sales Trends:")
+    else:
+        print("Unknown input. Try again.")
+        menu_selection(dbConn)
+
+def playlists_usage(dbConn):
+    print("Select an option: ")
+    print("  1. Print playlist track counts")
+    print("  2. Print most common media types")
+    cmd = input("Your choice --> ")
+    print()
+
+
+    if(cmd == '1'):
+        print("Number of Trakcs per Playlist:")
+    elif(cmd == '2'):
+        print("Most Common Media Types:")
+    else:
+        print("Unknown input. Try again.")
+        menu_selection(dbConn)
+
+
+
 
 def menu_selection(dbConn):
     print()
     print("Select a menu option: ")
     print("  1. Print general statistics about the database")
+    print("  2. Print information about music and tracks")
+    print("  3. Print information about albums and artists")
+    print("  4. Print information about sales and revenue")
+    print("  5. Print information about playlists and usage")
     print("or x to exit the program.")
     cmd = input("Your choice --> ")
     print()
 
     if(cmd == '1'):
         print_stats(dbConn)
+    elif(cmd == '2'):
+        music_tracks(dbConn)
+    elif(cmd == '3'):
+        albums_artists(dbConn)
+    elif(cmd == '4'):
+        sales_revenue(dbConn)
+    elif(cmd == '5'):
+        playlists_usage(dbConn)
     elif(cmd == 'x'):
         print("Exiting program.")
     else:
